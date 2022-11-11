a5ee9639c5eca9f9af1fd1e3c524440fa78f7448
qizhengmao <2801278540@qq.com>
2022-11-11 16:09:32 +0800

    Initial commit

---
+++ src/main/java/com/example/background_program/BackgroundProgramApplication.java
@@ -0,0 +1,13 @@
+package com.example.background_program;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackgroundProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackgroundProgramApplication.class, args);
+    }
+
+}
